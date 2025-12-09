1f80ee715a5990d7675e704b7cfb2fd4b602d784
Amadeu Arderiu <amadeu@sirvelia.com>
2025-12-09 17:11:18 +0100

    fix card components

--- src/app/(home)/components/FeaturesGrid.tsx
+++ src/app/(home)/components/FeaturesGrid.tsx
@@ -1,5 +1,5 @@
 import Container from "../../../components/Container";
-import { Card, CardContent, CardHeader, CardTitle } from "../../../components/ui/card";
+import { Card, CardContent } from "../../../components/ui/card";
 import Image from "next/image";
 
 export default function FeaturesGrid() {
